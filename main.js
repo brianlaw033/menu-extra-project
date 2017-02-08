b49b981ae5e958f6549671ec227712ab15120630
brianlaw033 <brianlaw@LAWtekiMacBook-Pro.local>
2017-02-08 16:56:23 +0800

    Ready to add Google Analytics

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+$('#navPress').click(function shownoshow(){
+  var myVar = $('#navToggle').hasClass('in');
+  if (myVar === true){
+    $('.navbar').removeAttr("background-color")
+    $('.navbar').css('background-color', 'transparent')
+  }
+  else {
+    $('.navbar').removeAttr("background-color")
+    $('.navbar').css('background-color', '#F15A29')
+  }
+})
+})
+
 $('.carousel .item').each(function(){
   var next = $(this).next();
   if (!next.length) {
@@ -11,17 +25,4 @@ $('.carousel .item').each(function(){
   else {
     $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
-});
-
-jQuery(function ($) {
-    $('.carousel').carousel();
-    var caption = $('div.item:nth-child(1) .carousel-caption');
-    $('.new-caption-area').html(caption.html());
-    caption.css('display', 'none');
-
-    $(".carousel").on('slide.bs.carousel', function (evt) {
-        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index() + 1) + ') .carousel-caption');
-        $('.new-caption-area').html(caption.html());
-        caption.css('display', 'none');
-    });
 });
